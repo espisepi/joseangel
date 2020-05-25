98b46510e4cf5c5e0aa4d78946c8ffacda9539f8
espisepi <sepinaco@gmail.com>
2020-05-26 01:41:09 +0200

    finalizada segunda animacion gatacattana

--- javascript-npm/01-gatacattana/index.js
+++ javascript-npm/01-gatacattana/index.js
@@ -485,13 +485,102 @@ function tween1() {
 
 }
 
+function tween2() {
+	// 48 segundos tenemos para lucirnos
 
-var animations = [ tween1 ];
-var numAnimaciones = 1;
+	// let group = new THREE.Group();
+	// group.add( scene.getObjectByName('water') );
+	// group.add( scene.getObjectByName('horse') );
+	// scene.add(group);
+
+
+	let posicionGatacattana = {
+		x: 442.94,
+		y: 111.44,
+		z: 1.93
+	};
+
+	let posicionCercaAguaLejosCaballo = {
+		x: 920.7504792035136,
+		y: 4.997434958552961,
+		z: 478.6020417334049
+	}
+
+	let fromPosition = {
+        x: posicionGatacattana.x,
+        y: posicionGatacattana.y,
+        z: posicionGatacattana.z
+    };
+
+    let posiciones = {
+    	x: [ posicionCercaAguaLejosCaballo.x,512.3417884392231, -578.8537004670098, posicionGatacattana.x ],
+    	y: [posicionCercaAguaLejosCaballo.y,posicionCercaAguaLejosCaballo.y,posicionCercaAguaLejosCaballo.y, posicionGatacattana.y],
+    	z: [posicionCercaAguaLejosCaballo.z,902.2332684245173, 861.0733837999686, posicionGatacattana.z]
+    }
+
+    let toPosition = {
+        x: posiciones.x[0],
+        y: posiciones.y[0],
+        z: posiciones.z[0]
+    };
+
+    let toPosition2 = {
+        x: posiciones.x[1],
+        y: posiciones.y[1],
+        z: posiciones.z[1]
+    };
+
+    let toPosition3 = {
+        x: posiciones.x[3],
+        y: posiciones.y[3],
+        z: posiciones.z[3]
+    };
+
+    let updatePosition = () => {
+    	camera.position.set(fromPosition.x, fromPosition.y, fromPosition.z);
+    };
+
+    let tweenPosition = new TWEEN.Tween(fromPosition)
+        .to(toPosition, 18000)
+        .easing(TWEEN.Easing.Linear.None)
+        .onUpdate(updatePosition)
+        .onComplete(function () {
+        	//controls.target.copy(scene.position);
+    	});
+
+    let tweenPosition2 = new TWEEN.Tween(fromPosition)
+        .to(toPosition2, 20000)
+        .easing(TWEEN.Easing.Linear.None)
+        .onUpdate(updatePosition)
+        .onComplete(function () {
+        	//controls.target.copy(scene.position);
+    	});
+
+   let tweenPosition3 = new TWEEN.Tween(fromPosition)
+        .to(toPosition3, 10000)
+        .easing(TWEEN.Easing.Linear.None)
+        .onUpdate(updatePosition)
+        .onComplete(function () {
+        	//controls.target.copy(scene.position);
+    	});
+
+    tweenPosition.chain(tweenPosition2);
+    tweenPosition2.chain(tweenPosition3);
+ 	tweenPosition.start();
+
+}
+
+
+var animations = [ tween1, tween2 ];
+animations.reverse();
+var numAnimaciones = 2;
 // Esta funcion es llamada por cada render()
 function checkTweenAnimation() {
 	if(animations.length != 0) {
 		if(video.currentTime > 3.0 && animations.length === numAnimaciones) {
+			(animations.pop())();
+		}
+		if(video.currentTime > 110.0 && animations.length === numAnimaciones - 1) {
 			(animations.pop())();
 		}
 	}
