dee75bcd69ca6ab88abe559c99e50dba8bdf47c3
espisepi <sepinaco@gmail.com>
2020-07-07 00:32:50 +0200

    03-aframe-> 07-realistic: recolocando camara, 05-animacion: comentando codigo para mover al personaje

--- javascript-npm/03-aframe/components/animation-nathan.js
+++ javascript-npm/03-aframe/components/animation-nathan.js
@@ -12,7 +12,7 @@ AFRAME.registerComponent('animation-nathan', {
         // console.log(this.el.components["animation-mixer"]);
         console.log(this.el.getAttribute('animation-mixer'));
         const durationAnimation = 3;
-        this.el.setAttribute('animation-mixer', {loop:'infinite', duration:durationAnimation, timeScale: 0});
+        this.el.setAttribute('animation-mixer', {loop:'infinite', duration:durationAnimation, timeScale: 1});
         
         // Caja morada de ayuda
         this.boxEl = document.createElement('a-box');
@@ -28,58 +28,58 @@ AFRAME.registerComponent('animation-nathan', {
         this.el.addEventListener('animation-loop', function(){
             el.object3D.getWorldDirection(direction);
             el.object3D.position.addScaledVector(direction, 3.0);
-            if(stopAnimation){
-                el.setAttribute('animation-mixer', {timeScale: 0});
-            }else{
-                console.log(self.input);
+            // if(stopAnimation){
+            //     el.setAttribute('animation-mixer', {timeScale: 0});
+            // }else{
+            //     console.log(self.input);
 
-                // Calculamos la rotacion segun los valores del input
+            //     // Calculamos la rotacion segun los valores del input
                 
 
-            }
+            // }
         });
 
         document.addEventListener('keydown', function (evt) {
-            if(evt.key === "a"){
-                input.horizontal = -1;
-                el.object3D.rotation.y = -Math.PI / 2;
-                stopAnimation = false;
-                el.setAttribute('animation-mixer', {timeScale: 1});
-            }
-            if(evt.key === "w"){
-                input.vertical = 1;
-                el.object3D.rotation.y = 0;
-                stopAnimation = false;
-                el.setAttribute('animation-mixer', {timeScale: 1});
-            }
-            if(evt.key === "s"){
-                input.vertical = -1;
-                el.object3D.rotation.y = Math.PI;
-                stopAnimation = false;
-                el.setAttribute('animation-mixer', {timeScale: 1});
-            }
-            if(evt.key === "d"){
-                input.horizontal = 1;
-                el.object3D.rotation.y = Math.PI / 2;
-                stopAnimation = false;
-                el.setAttribute('animation-mixer', {timeScale: 1});
-            }
+            // if(evt.key === "a"){
+            //     input.horizontal = -1;
+            //     el.object3D.rotation.y = -Math.PI / 2;
+            //     stopAnimation = false;
+            //     el.setAttribute('animation-mixer', {timeScale: 1});
+            // }
+            // if(evt.key === "w"){
+            //     input.vertical = 1;
+            //     el.object3D.rotation.y = 0;
+            //     stopAnimation = false;
+            //     el.setAttribute('animation-mixer', {timeScale: 1});
+            // }
+            // if(evt.key === "s"){
+            //     input.vertical = -1;
+            //     el.object3D.rotation.y = Math.PI;
+            //     stopAnimation = false;
+            //     el.setAttribute('animation-mixer', {timeScale: 1});
+            // }
+            // if(evt.key === "d"){
+            //     input.horizontal = 1;
+            //     el.object3D.rotation.y = Math.PI / 2;
+            //     stopAnimation = false;
+            //     el.setAttribute('animation-mixer', {timeScale: 1});
+            // }
           });
         document.addEventListener('keyup', function (evt) {
-            stopAnimation = true;
+            // stopAnimation = true;
 
-            if(evt.key === "a"){
-                input.horizontal = 0;
-            }
-            if(evt.key === "w"){
-                input.vertical = 0;
-            }
-            if(evt.key === "s"){
-                input.vertical = 0;
-            }
-            if(evt.key === "d"){
-                input.horizontal = 0;
-            }
+            // if(evt.key === "a"){
+            //     input.horizontal = 0;
+            // }
+            // if(evt.key === "w"){
+            //     input.vertical = 0;
+            // }
+            // if(evt.key === "s"){
+            //     input.vertical = 0;
+            // }
+            // if(evt.key === "d"){
+            //     input.horizontal = 0;
+            // }
         });
 
         
